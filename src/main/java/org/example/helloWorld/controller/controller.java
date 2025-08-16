c2cdb51ef145d39d12cc2f1b4301e6e767735f01
Manoj Taraka Ramarao Gunuru <manoj.gtr@iitgn.ac.in>
2025-08-16 19:10:36 +0530

    basic rendering implemented

---
+++ src/main/java/org/example/helloWorld/controller/controller.java
@@ -0,0 +1,15 @@
+package org.example.helloWorld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class controller {
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("message", "Hello World from HelloWorld service!");
+        return "index"; // resolves to src/main/resources/templates/index.html
+    }
+}
